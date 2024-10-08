723117fef235416208aa0349a5cdc8774b9edd12
Matthew Wiblin <matthew@wibles.net>
2024-10-08 10:19:06 +0100

    first JS Page

--- first_intro/index.js
+++
@@ -1,7 +0,0 @@
-let username;
-
-document.getElementById("mysubmit").onclick = function(){
-    username = document.getElementById("mytext").value
-    document.getElementById("P1").textContent = username
-}
-
