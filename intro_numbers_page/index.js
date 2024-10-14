e5c87258816eac6def400608b3afdb0f39384452
Matthew Wiblin <matthew@wibles.net>
2024-10-14 16:07:31 +0100

    calc completed

--- intro_numbers_page/index.js
+++ intro_numbers_page/index.js
@@ -121,3 +121,22 @@ while(running){
 
 */
 
+// CALCULATOR
+
+const display = document.getElementById("calcDisplay")
+
+function appendToDisplay(input){
+    display.value += input
+}
+
+function clearDisplay(){
+    display.value = ""
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value)
+    }catch(error){
+        display.value = "Error"
+    }
+}
